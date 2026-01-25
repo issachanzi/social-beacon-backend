31f14bd5e9a1d49142a78909967bbce1dea2bc23
Issa Chanzi <s3783615@student.rmit.edu.au>
2026-01-25 12:32:05 +1100

    Update version of dependency rest-easy

--- src/main/java/net/issachanzi/beacon/model/User.java
+++ src/main/java/net/issachanzi/beacon/model/User.java
@@ -39,7 +39,7 @@ public class User extends EasyModel {
         }
         else if (this.displayName == null) {
             throw new BadRequest("Display name must not be null");
-        }
+        }   
     }
 
     public void password (String password) {
